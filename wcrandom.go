2160371f7a215a3dc99212d438e3f8d8048a5d91
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-12-25 23:24:24 -0600

    circular queue of strings

--- wcrandom.go
+++ wcrandom.go
@@ -53,6 +53,43 @@ func showWordCounts(wc map[string]int, showTop int) {
 	fmt.Printf("words { %s }\n", strings.Join(pretty, ", "))
 }
 
+// CQueueString is a circular queue
+type CQueueString struct {
+	queue                 []string
+	storePos, retrievePos int
+	count                 int
+}
+
+func (cq *CQueueString) init(size int) {
+	cq.queue = make([]string, size)
+	cq.storePos = 0
+	cq.retrievePos = 0
+	cq.count = 0
+}
+func (cq *CQueueString) add(s string) int {
+	if cq.count > len(cq.queue) {
+		return -1
+	}
+	cq.queue[cq.storePos] = s
+	cq.storePos = (cq.storePos + 1) % len(cq.queue)
+	cq.count++
+	return cq.count
+}
+
+func (cq *CQueueString) remove() (int, string) {
+	if cq.count <= 0 {
+		return -1, ""
+	}
+	item := cq.queue[cq.retrievePos]
+	cq.retrievePos = (cq.retrievePos + 1) % len(cq.queue)
+	cq.count--
+	return cq.count, item
+}
+
+func (cq *CQueueString) isFull(s string) bool {
+	return len(cq.queue) == cq.count
+}
+
 func driver(config *WordCountConfig) {
 	nextWord := wordGenerator("unique", config.uniqueWords)
 	queue := make([]string, config.lastNWords)
@@ -65,6 +102,7 @@ func driver(config *WordCountConfig) {
 		if len(queue) >= config.lastNWords {
 			droppedWord := queue[0]
 			queue = queue[1:]
+			fmt.Printf("queue len = %d, cap = %d", len(queue), cap(queue))
 			wc[droppedWord]--
 			if wc[droppedWord] <= 0 {
 				delete(wc, droppedWord)
