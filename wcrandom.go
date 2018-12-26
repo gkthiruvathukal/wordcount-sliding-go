4d307f9c66de2b627dcd161996bcd5caee41e673
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-12-26 16:31:19 -0600

    some cleanup of showWordCounts presentation layer

--- wcrandom.go
+++ wcrandom.go
@@ -9,13 +9,9 @@ import (
 	"strings"
 )
 
-// WordCountConfig The configuration options available.
 type WordCountConfig struct {
 	uniqueWords, lastNWords, wordsToGenerate, showTop int
 }
-
-// newWord() shows how to build an iterator on string
-// can make a stateful iterator by putting some variables before the return...
 
 func wordGenerator(wordBase string, uniqueWords int) func() string {
 	return func() string {
@@ -24,28 +20,39 @@ func wordGenerator(wordBase string, uniqueWords int) func() string {
 	}
 }
 
+type WC struct {
+	word  string
+	count int
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func showWordCounts(wc map[string]int, showTop int) {
-	type WC struct {
-		word  string
-		count int
-	}
 
-	wcSlice := make([]WC, len(wc))
-	wcSlice = wcSlice[:0]
+	// wcData is a slice of (word, count) pairs (as struct WC)
+	wcData := make([]WC, len(wc))
+	i := 0
 	for word, count := range wc {
-		wcSlice = append(wcSlice, WC{word, count})
-	}
-	sort.Slice(wcSlice, func(i, j int) bool {
-		return wcSlice[i].count > wcSlice[j].count
-	})
-	if len(wcSlice) > showTop {
-		wcSlice = wcSlice[:showTop]
+		wcData[i] = WC{word, count}
+		i++
 	}
 
-	pretty := make([]string, len(wcSlice))
-	pretty = pretty[:0]
-	for _, wc := range wcSlice {
-		pretty = append(pretty, fmt.Sprintf("%s: %d", wc.word, wc.count))
+	// sort.Slice() sorts by count field, descending
+	sort.Slice(wcData, func(i, j int) bool {
+		return wcData[i].count > wcData[j].count
+	})
+
+	// create slice of strings for presentation as JSON like output
+	numberToPrint := min(len(wcData), showTop)
+	pretty := make([]string, numberToPrint)
+
+	for i := 0; i < numberToPrint; i++ {
+		pretty[i] = fmt.Sprintf("%s: %d", wcData[i].word, wcData[i].count)
 	}
 	fmt.Printf("words { %s }\n", strings.Join(pretty, ", "))
 }
@@ -79,6 +86,5 @@ func main() {
 	flag.IntVar(&config.wordsToGenerate, "generate", config.wordsToGenerate, "words to generate randomly")
 	flag.IntVar(&config.showTop, "show_top", config.showTop, "show top n words")
 	flag.Parse()
-	fmt.Println(config)
 	driver(&config)
 }
