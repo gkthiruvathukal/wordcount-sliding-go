7a39122bf2edd1bb0a9e103b136ba73f576a8614
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-12-26 09:47:55 -0600

    created simple circular queue implementation

---
+++ cqueue.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// CQueueString is a circular queue
+
+type CQueueString struct {
+	queue                 []string
+	storePos, retrievePos int
+	count                 int
+}
+
+func (cq *CQueueString) show() {
+	fmt.Printf("storePos = %d, retrievePos = %d, queue = ", cq.storePos, cq.retrievePos)
+	fmt.Println(cq.queue)
+}
+
+func (cq *CQueueString) init(size int) {
+	cq.queue = make([]string, size)
+	cq.storePos = 0
+	cq.retrievePos = 0
+	cq.count = 0
+}
+func (cq *CQueueString) add(s string) int {
+	if cq.isFull() {
+		return -1
+	} else {
+		cq.queue[cq.storePos] = s
+		cq.storePos = (cq.storePos + 1) % len(cq.queue)
+		cq.count++
+		return cq.count
+	}
+}
+
+func (cq *CQueueString) remove() (int, string) {
+	if cq.isEmpty() {
+		return -1, ""
+	} else {
+		item := cq.queue[cq.retrievePos]
+		cq.retrievePos = (cq.retrievePos + 1) % len(cq.queue)
+		cq.count--
+		return cq.count, item
+	}
+}
+
+func (cq *CQueueString) isFull() bool {
+	return len(cq.queue) == cq.count
+}
+func (cq *CQueueString) isEmpty() bool {
+	return len(cq.queue) == 0
+}
--- wcrandom.go
+++ wcrandom.go
@@ -42,9 +42,6 @@ func showWordCounts(wc map[string]int, showTop int) {
 		wcSlice = wcSlice[:showTop]
 	}
 
-	//fmt.Printf("wc len %d, show top %d, slice len %d, slice cap %d", len(wc), showTop, len(wcSlice), cap(wcSlice))
-	// printing words...
-
 	pretty := make([]string, len(wcSlice))
 	pretty = pretty[:0]
 	for _, wc := range wcSlice {
@@ -53,62 +50,23 @@ func showWordCounts(wc map[string]int, showTop int) {
 	fmt.Printf("words { %s }\n", strings.Join(pretty, ", "))
 }
 
-// CQueueString is a circular queue
-type CQueueString struct {
-	queue                 []string
-	storePos, retrievePos int
-	count                 int
-}
-
-func (cq *CQueueString) init(size int) {
-	cq.queue = make([]string, size)
-	cq.storePos = 0
-	cq.retrievePos = 0
-	cq.count = 0
-}
-func (cq *CQueueString) add(s string) int {
-	if cq.count > len(cq.queue) {
-		return -1
-	}
-	cq.queue[cq.storePos] = s
-	cq.storePos = (cq.storePos + 1) % len(cq.queue)
-	cq.count++
-	return cq.count
-}
-
-func (cq *CQueueString) remove() (int, string) {
-	if cq.count <= 0 {
-		return -1, ""
-	}
-	item := cq.queue[cq.retrievePos]
-	cq.retrievePos = (cq.retrievePos + 1) % len(cq.queue)
-	cq.count--
-	return cq.count, item
-}
-
-func (cq *CQueueString) isFull(s string) bool {
-	return len(cq.queue) == cq.count
-}
-
 func driver(config *WordCountConfig) {
 	nextWord := wordGenerator("unique", config.uniqueWords)
-	queue := make([]string, config.lastNWords)
+	queue := new(CQueueString)
+	queue.init(config.lastNWords)
 	wc := make(map[string]int)
 
-	queue = queue[:0]
 	for i := 0; i < config.wordsToGenerate; i++ {
 		word := nextWord()
-		//fmt.Printf("queue length %d\n", len(queue))
-		if len(queue) >= config.lastNWords {
-			droppedWord := queue[0]
-			queue = queue[1:]
-			fmt.Printf("queue len = %d, cap = %d", len(queue), cap(queue))
+		//queue.show()
+		if queue.isFull() {
+			_, droppedWord := queue.remove()
 			wc[droppedWord]--
 			if wc[droppedWord] <= 0 {
 				delete(wc, droppedWord)
 			}
 		}
-		queue = append(queue, word)
+		queue.add(word)
 		wc[word]++
 		showWordCounts(wc, config.showTop)
 	}
