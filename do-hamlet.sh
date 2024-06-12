f4052d7680792bd42b1371a9c98719c9a37ae474
George K. Thiruvathukal <gthiruvathukal@luc.edu>
2024-06-11 23:29:10 -0500

    tuning by buffer size

--- do-hamlet.sh
+++ do-hamlet.sh
@@ -6,7 +6,13 @@ if [ ! -d build ]; then
 fi
 
 if [ -f "build/sliding_wordcount" ]; then
-   cat data/hamlet-10-copies.txt | ./build/sliding_wordcount
+   echo "Sequential"
+   time cat data/hamlet-10-copies.txt | ./build/sliding_wordcount > /dev/null
+   echo "Functional Style / Channels / Varying buffer size"
+   for BUFFER_SIZE in 10 100 1000 10000 100000 1000000; do
+      echo "buffer size", $BUFFER_SIZE
+      time cat data/hamlet-10-copies.txt | ./build/sliding_wordcount -go-routines -channel-size $BUFFER_SIZE > /dev/null
+   done
 else
    echo "No executable"
    exit 2
